939fe0987be912393b6301a2351aaea9359fd39c
rajnish0209 <rajnishkumargupta331@gmail.com>
2025-04-27 18:55:09 +0530

    spiral

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-function isPrime(n) {
-  if (n <= 1) return false;
-  for (let i = 2; i < Math.sqrt(n); i++) {
-    if (n % i === 0) return false;
-  }
-  return true;
-}
+let m = [
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9, 10],
+  [11, 12, 13, 14, 15],
+  [16, 17, 18, 19, 20],
+];
+
+function spiralPrint(arr) {}
+
+spiralPrint(m);
